143dc64c15057a801eb1f63a6f525164c12ffa14
sthsf <liyu52577@163.com>
2023-07-18 22:30:29 +0800

    code updatet

--- src/scripts/preprocess_dataset.sh
+++ src/scripts/preprocess_dataset.sh
@@ -25,6 +25,8 @@
 # CMD="python /yinxr/deepq/CPM-Bee/src/preprocess_dataset.py --input /yinxr/deepq/raw_data_mlm/书籍 --output_path /yinxr/deepq/CPM-Bee/data/pretrain/mlm --output_name pretrain"
 
 ## jeeves
+# CMD="python /data/CPM-Bee/src/preprocess_dataset_en.py --input /data/pretrain_data/shenqing_financial_data/mlm_data  --output_path /data/pretrain_data/shenqing_financial_data/pretrain/mlm --output_name pretrain"
+CMD="python /data/CPM-Bee/src/preprocess_dataset_en.py --input /data/pretrain_data/shenqing_financial_data/lm_data  --output_path /data/pretrain_data/shenqing_financial_data/pretrain/lm --output_name pretrain"
 
 
 
